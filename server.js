49d529a937c859834552836b374605a12e20cf31
surenmahi <36105235+surenmahi@users.noreply.github.com>
2018-02-16 12:31:17 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,27 +1,95 @@
 var express = require('express');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' })
 var app = express();
-var logger = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var logger = require('morgan');
 var path = require('path');
-var http = require('http');
+var server=require('http').Server(app);
+var io=require('socket.io')(server);
+const port=process.env.PORT||3000;
 
 
-app.use(logger('dev'));
+var tariffRoute=require('./server/routes/tariff');
+var userRoute=require('./server/routes/user');
+var cabRoute=require('./server/routes/cab');
+var bookingRoute=require('./server/routes/booking');
+
+
+
+
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,'/client')));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, '/client')));
 
 
-// app.listen(3000,function(req,res){
-//   console.log('Server is running on port 3000');
-// });
-app.set('port',(process.env.PORT || 3000));
-app.listen(app.get('port'),function(){
-  console.log('Server is running');
+mongoose.connect('mongodb://localhost/meanapp');
+var db=mongoose.connection;
+db.on('error',console.error.bind(console,'connection error'));
+db.once('open',function(){
+  console.log('Database Connected');
 });
-// http.createServer(function (req,res){
-//   res.writeHead(200,{'Content-Type':'text/plain'});
-//   res.end('Hello');
-// }).listen(3000,'103.81.236.222');
-// app.listen(3000,'103.81.236.222' || 'localhost', function(){
-//   console.log('Server started');
-// });
+
+app.use('/', tariffRoute);
+app.use('/', cabRoute);
+app.use('/', userRoute);
+app.use('/', bookingRoute);
+
+
+
+
+io.on('connection',function(socket){
+console.log('Socket: ',socket.id);
+io.sockets.emit('BookingID',socket.id);
+socket.on('getLocation',function(data){
+  console.log(data);
+  io.sockets.emit('sendLocation',data);
+});
+socket.on('getBookingID',function(data){
+  console.log(data);
+  io.emit('sendBookingID',socket.id);
+});
+
+
+socket.on('getDriverInfo',function(data){
+  console.log(data);
+  io.sockets.emit('sendDriverInfo',data);
+});
+socket.on('getBookingInfo',function(data){
+  console.log(data);
+  io.sockets.emit('sendBookingInfo',data);
+});
+socket.on('CancelBooking',function(data){
+  console.log(data);
+  io.sockets.emit('CancelBooking',data);
+});
+
+
+
+});
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./client/public/images");
+    },
+    filename: function (req, file, callback) {
+        callback(null,file.originalname);
+    }
+});
+
+var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count
+
+
+
+app.post("/api/Upload", function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end("Something went wrong!");
+        }
+        return res.end("File uploaded sucessfully!.");
+    });
+});
+
+server.listen(port,function(req,res){
+  console.log(`Server is running on port ${port}...`);
+});
